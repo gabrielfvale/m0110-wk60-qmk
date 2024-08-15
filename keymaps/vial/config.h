eaf82f9f03c605407ac850ce078ae8067ede6a3e
Gabriel Vale <gabrielfvale@users.noreply.github.com>
2024-08-15 13:03:42 -0300

    feat: vial keymap

---
+++ keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xA2, 0x5D, 0x4B, 0x52, 0xFC, 0xFE, 0x32, 0x25}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 2, 12 }
