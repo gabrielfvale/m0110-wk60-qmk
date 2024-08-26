19b28fe3af895b3a9557fcbd7d31cc5b21cc75d8
Gabriel Vale <gabrielfvale@users.noreply.github.com>
2024-08-26 14:37:23 -0300

    fix: full layout mapping

--- keymaps/vial/config.h
+++ keymaps/vial/config.h
@@ -4,5 +4,5 @@
 
 #define VIAL_KEYBOARD_UID {0xA2, 0x5D, 0x4B, 0x52, 0xFC, 0xFE, 0x32, 0x25}
 
-#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
-#define VIAL_UNLOCK_COMBO_COLS { 2, 12 }
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 12 }
